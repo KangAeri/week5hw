0717e50b38c4ea93daa48ca74b60a7b8d3f97f71
unknown <rkddofl12355@gmail.com>
2022-04-08 08:26:49 +0900

    b2

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
 	printf("The number of testcase : ");
 	scanf("%d", &num);			//입력 첫줄 : test case의 개수를 받아 num에 저장함  
 	for(i=0 ; i<num ; i++){		//페이지수 입력받아 계산하는 과정을 test case의 개수만큼  반복 
-		printf("page? : ");
+		printf("put the number of the pages : ");
 		scanf("%d", &page);		//페이지 수를 입력받음 
 		numofeach(page, countarr);		//계산을 실행하는 numofeach 함수 호출, 인자로 page와 countarr배열을 넘김 
 		printnum(countarr);				//계산된 결과를 저장한 배열의 내용을 출력하는 함수 printnum 함수 호출  
